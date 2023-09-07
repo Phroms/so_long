79290c5b598ba1c28e0aa7abfe3dcd7dff7cd3ab
Phroms <hernangrim.10@gmail.com>
2023-09-07 21:50:05 +0200

    leyendo mapa

--- src/errors.c
+++ src/errors.c
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/05 17:12:57 by agrimald          #+#    #+#             */
-/*   Updated: 2023/09/07 21:31:12 by agrimald         ###   ########.fr       */
+/*   Created: 2023/09/07 19:34:39 by agrimald          #+#    #+#             */
+/*   Updated: 2023/09/07 20:35:14 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "../../so_long.h"
 
-#include <unistd.h> // open, close, read, write
-#include <stdlib.h>	// malloc, free 
-#include <stdio.h>	// perror
-
-
-/* Funciones */
-
-	/* map.c */
-
-void	read_map(int fd, t_game *game);
-void	analyze_map(t_game *game);
-
-	/* errors.c */
-
-void	exit_nocompilation(void);
-#endif
+void	exit_nocompilation(void)
+{
+	ft_printf("Error el mapa no compila o esta vacio");
+	exit(EXIT_FAILURE);
+}
--- src/map.c
+++ src/map.c
@@ -6,13 +6,13 @@
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:08:59 by agrimald          #+#    #+#             */
-/*   Updated: 2023/09/07 17:35:10 by agrimald         ###   ########.fr       */
+/*   Updated: 2023/09/07 21:49:40 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-void	read_map(int fd, t_game *d)
+void	read_map(int fd, t_game *game)
 {
 	int	*line;
 	int	*tmp_line;
@@ -27,7 +27,61 @@ void	read_map(int fd, t_game *d)
 		if (line && line[0] == '\0')
 		{
 			free(line);
+			exit_nocompilation();
 		}
 		tmp_line = all_line;
+		all_line = ft_strjoin(tmp_line, line);
+		free(tmp_line);
 	}
+	if (ft_strcmp(all_line, "") == 0)
+		exit_no_compilation();
+	game->map = ft_split(all_line, '\n');
+	game->map_len = ft_split(all_line, '\n');
+	free(all_line);
+}
+
+void	analyze_map(t_game *game)
+{
+	// verifica si el mapa es rectangular;
+	if (map_is_rect(game) == 0)
+		map_exit(game->map, NOT_RECT, "EL mapa no es rectangulo"); // hacer una funcion para "map exit" en donde le enviaremos la direccion de "map", un enuncidao y un str;
+	
+	// verifica si hay problemas con la cantidad de paredes del mapa;
+	else if(map_view_walls(game) > 0)
+		map_exit(game->map, NOT_WALLS, "El tamano del mapa es incorrecto"); // lo mismo pero para "map view walls";
+
+	// verifica si hay caracteres duplicados o si hay alguno faltante;
+	else if (view_duplicate_chars(game) == 0)
+		map_exit(game->map, NOT_CHARS, "Hay caracteres duplicados y/o faltantes"); // lo mismo para "view duplicate chars";
+	
+	// verifica si hay caracteres invalidos;
+	else if (incorret_chars_in_map(game) == 0)
+		map_exit(game->map, BAD_CHARS, "Hay caracteres invalidos"); // lo mismo para "incorret chars in map";
+
+	// verifica si hay backtraking
+	map_backtraking(game); // sera otra funcion para ver si hay backtraking o no;
+}
+
+void	map_star(char **argv, t_game *game)
+{
+	int len;
+	char *str;
+	int fd;
+
+	str = argv[1];
+	len = ft_strlen(str);
+	if (!(str[len - 1] == 'r' && str[len - 2] == 'e'&& str[len - 3] == 'b' \
+			&& str[len - 4] == '.'))
+	{
+		ft_printf("Error al ejecutar el programa");
+		exit(1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (!fd)
+		exit (1);
+	ft_printf("ACTUALIZACION: ARCHIVO ABIERTO CON EXITO EN EL FD %d\n", fd);
+	read_map(fd, game);
+	analyze_map(game);
+	ft_printf("ACTUALIZACION:MAPA ANALIZADO CORRECTAMENTE\n");
+	close(fd);
 }
