280240a435faaf968e9ea47881312a97ec4e65ef
Phroms <hernangrim.10@gmail.com>
2023-09-13 18:30:00 +0200

    aaaa

--- src/player_movements.c
+++ src/player_movements.c
@@ -6,13 +6,32 @@
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 18:47:25 by agrimald          #+#    #+#             */
-/*   Updated: 2023/09/12 18:56:04 by agrimald         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:28:57 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-void	player movements(t_game *game)
+void	player_movements(t_game *game, int row_offest, int col_offset)
 {
-	
+	if (valid_movements(game, game->m_row + row_offset, game->m_col + col_offset))
+	{
+		taking_coins(game, game->m_row + row_offset, game->m_col + col_offset);
+		if (game->map[game->m_row + row_offset][game->m_col + col_offset] == 'E')
+		{
+			mlx_put_image_to_window(game->mlx, game->win, game->img[3].img_ptr, \
+					(game->m_col + col_offset) * 32, (game->m_row + row_offset * 32));
+		}
+		else
+		{
+			mlx_put_image_to_window(game->mlx, game->win, game->img[2].img_ptr, \
+					(game->m_col + col_offset) * 32, (game->m_row + row_offset * 32));
+		}
+		game->m_row += row_offset;
+		game->m_col += col_offset;
+		mlx_put_image_to_win(game->mlx, game->win, game->img[4].img_ptr, game->m_col * 32, game->m_row * 32);
+		game_finish(game, game->m_row, game->m_col);
+		ft_printf("Player moves %d\n", game->moves);
+		game->moves += 1;
+	}
 }
